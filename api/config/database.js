f4f7cd6d43665be2512989795a62b1cc9b345a80
Gaurav Mehra <gauravmehra1987@gmail.com>
2022-03-15 10:45:09 +0530

    Adding mysql connection

--- api/config/database.js
+++ api/config/database.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
+  // connection: {
+  //   client: 'sqlite',
+  //   connection: {
+  //     filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //   },
+  //   useNullAsDefault: true,
+  // },
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+    client: 'mysql',
+    debug: false,
     useNullAsDefault: true,
-  },
+    connection: {
+      host      : 'db',
+      port      : 3306,
+      user      : 'root',
+      password  : 'root',
+      database  : 'walls',
+    }
+  }
 });
